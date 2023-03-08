ce8be2b6181f1d4f38b6b9c6724ea8c40ec14c05
Michael Huber <michael.huber1981@outlook.com>
2023-03-08 11:06:03 -0600

    Adding getQuestionUomValues function

---
+++ functions/Questions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { isRequired } from "./General.js";
+
+// Get a list of valid answers for a Question and unit of measure
+export async function getQuestionUomValues(
+  authToken = isRequired("authToken"),
+  env = isRequired("env (NP or PROD)"),
+  qCode = "PATHOGEN",
+  uom = "SCINAME"
+) {
+  try {
+    if (env.toUpperCase() !== "NP" && env.toUpperCase() !== "PROD") {
+      throw new Error(
+        `${env} is not a valid argument, please use "NP" or "PROD".`
+      );
+    }
+
+    const response = await axios.get(
+      env === "NP"
+        ? process.env.VALIDATION_ENDPOINT +
+            `${qCode.toUpperCase()}/uom/${uom.toUpperCase()}`
+        : process.env.PROD_VALIDATION_ENDPOINT +
+            `${qCode.toUpperCase()}/uom/${uom.toUpperCase()}`,
+      {
+        headers: { Authorization: `Bearer ${authToken.access_token}` },
+      }
+    );
+    if (response.status != 200) {
+      throw new Error(
+        `Status: ${response.status}, Status Text: ${response.statusText}`
+      );
+    } else {
+      console.log(
+        `${
+          response.data.rule.values.length
+        } answers for ${qCode.toUpperCase()} found.`
+      );
+      return response.data.rule.values;
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+}
