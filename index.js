bfde3d180dd81c9d83dea9f1f91414a10c26cd8d
Michael Huber <michael.huber1981@outlook.com>
2023-03-10 09:07:37 -0600

    Updating/improving primary example

--- index.js
+++ index.js
@@ -7,18 +7,35 @@ import { getQuestionUomValues } from "./functions/Questions.js";
 const csvFileName = "test";
 // Get token
 const authToken = await getAuthToken("NP");
+let csvArray;
 
 try {
   // Read CSV file
+  console.log(`Reading ${csvFileName}.csv`);
   const csvData = fs.readFileSync(`./${csvFileName}.csv`, "utf-8");
   // Covert to Array
-  const csvArray = CSVToArray(csvData, ",", true);
+  csvArray = CSVToArray(csvData, ",", true);
 
+  console.log(`Converting ${csvFileName}.csv to an array:`);
   console.log(csvArray);
 } catch (error) {
   console.log(`${error.message} (check csvFilename - line 6)`);
 }
 
-getQuestionUomValues(authToken, "NP").then((response) =>
-  console.log(`The first answer in the list is ${response[0].value}`)
-);
+for (let index = 0; index < csvArray.length; index++) {
+  const element = csvArray[index];
+  try {
+    console.log(
+      `Searching for answers to Question code ${element[0]} and UOM ${element[1]}...`
+    );
+    const response = await getQuestionUomValues(
+      authToken,
+      "NP",
+      element[0],
+      element[1]
+    );
+    console.log(`The first answer in the list is ${response[0].value}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
