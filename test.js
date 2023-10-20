e77535a502258c7cf95dae2681621d5de59b9ce5
mshuber1981 <michael.huber1981@outlook.com>
2023-10-20 08:27:16 -0500

    Updating console log color for warnings

--- test.js
+++ test.js
@@ -5,13 +5,19 @@ import { execPromise } from "./utils/general.js";
 try {
   const envFile = process.env.API_URL;
   if (envFile === undefined) {
-    console.log("Warning, part of this project relies on at least one URL from a local .env file.");
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      "Warning, part of this project relies on at least one URL from a local .env file."
+    );
   } else {
     console.log("Success, local .env file API_URL found.");
   }
   const vault = await execPromise("vault -v");
   console.log(vault);
 } catch (error) {
-  console.log("Warning, part of this project relies on the Vault cli to access client ids and secrets.");
+  console.warn(
+    "\x1b[33m%s\x1b[0m",
+    "Warning, part of this project relies on the Vault cli to access client ids and secrets."
+  );
   console.log(error);
 }
