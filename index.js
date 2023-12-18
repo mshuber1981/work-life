e062c469a479e4ef61dd207eacd3f7820acea86b
Michael Huber <michael.huber1981@outlook.com>
2023-12-18 12:21:09 -0600

    Adding BigQuery example

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import * as fs from "fs";
-set_fs(fs);
-import * as dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import axios from "axios";
 import { writeFile, utils, set_fs } from "xlsx";
+import { BigQuery } from "@google-cloud/bigquery";
 import csvToArray from "./utils/csv.js";
 
 // Set CSV file name
 const csvFileName = "test";
 let csvArray;
+const bigquery = new BigQuery();
+const query = ``;
 
+set_fs(fs);
 try {
   // Read CSV file
   console.log(`Reading ${csvFileName}.csv`);
@@ -37,7 +39,7 @@ try {
   });
   // Create worksheet
   const worksheet = utils.json_to_sheet(data);
-  // Add NP hyperlinks to mappings worksheet
+  // Add hyperlinks to worksheet
   for (let index = 0; index < result.data.length; index++) {
     const element = result.data[index];
     worksheet[`A${index + 2}`].l = {
@@ -54,4 +56,28 @@ try {
   writeFile(workbook, "My_GitHub_repos.xlsx");
 } catch (error) {
   console.log(error.code);
+}
+
+try {
+  const data = [];
+  console.log("Querying ISO Country Codes...");
+  const [job] = await bigquery.createQueryJob(query);
+  const [result] = await job.getQueryResults();
+  // Worksheet
+  data = [...result];
+  const worksheet = utils.json_to_sheet(data);
+  // Add hyperlinks to worksheet
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+    worksheet[`B${index + 2}`].l = {
+      Target: `https://www.iso.org/obp/ui/#iso:code:3166:${element.Two_Char_Code}`,
+    };
+  }
+  // Append NP worksheet to workbook
+  utils.book_append_sheet(workbook, worksheet, "ISO Country Codes");
+  // Export workbook
+  console.log("Exporting results to ISO_Country_Codes.xlsx");
+  writeFile(workbook, "ISO_Country_Codes.xlsx");
+} catch (error) {
+  console.log(error);
 }
