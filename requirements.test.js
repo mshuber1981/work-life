fcaa4577a240b9ea71f2e4667a0f6c75da8f28e2
Michael Huber <michael.huber1981@outlook.com>
2023-12-22 16:35:19 -0600

    Updating

--- requirements.test.js
+++ requirements.test.js
@@ -5,16 +5,16 @@ import { execPromise } from "./utils/general.js";
 test("Checking for .env API_URL variable...", () => {
   expect(typeof process.env.API_URL).toBe("string");
 });
-// 
-test("Checking for .env oauth related variables...", () => {
-  expect(typeof process.env.NP_SECRETS).toBe("string");
-  expect(typeof process.env.PROD_SECRETS).toBe("string");
-  expect(typeof process.env.OAUTH_ENDPOINT).toBe("string");
-});
 // Check for Vault CLI
 test("Checking for Vault CLI...", async () => {
   const vCli = await execPromise("vault -v");
   expect(typeof vCli).toBe("string");
+});
+// Check for .env OAuth related variables
+test("Checking for .env OAuth related variables...", () => {
+  expect(typeof process.env.NP_SECRETS).toBe("string");
+  expect(typeof process.env.PROD_SECRETS).toBe("string");
+  expect(typeof process.env.OAUTH_ENDPOINT).toBe("string");
 });
 // Check for Google Cloud CLI
 test("Checking for Google Cloud CLI...", async () => {
